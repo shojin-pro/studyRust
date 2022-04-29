ae09baa9dca7d7058ecb83804250003399145d5c
shojin-pro <shojin.debu@gmail.com>
2022-04-29 23:22:22 +0900

    thiserror,anyhowにおける実装の追加

--- samplecli/src/bin/main.rs
+++ samplecli/src/bin/main.rs
@@ -1,3 +1,5 @@
+use anyhow::{bail,ensure,Context,Result};
+
 use clap::Parser;
 use std::fs::File;
 use std::io::{stdin,BufReader,BufRead};
@@ -24,28 +26,31 @@ impl RpnCalculator{
         Self(verbose)
     }
 
-    pub fn eval(&self,formula: &str) -> i32{
+    pub fn eval(&self,formula: &str) -> Result<i32>{
         let mut tokens = formula.split_whitespace().rev().collect::<Vec<_>>();
         println!("{:?}", tokens);
         self.eval_inner(&mut tokens)
     }
 
-    fn eval_inner(&self, tokens : &mut Vec<&str>) -> i32 {
+    fn eval_inner(&self, tokens : &mut Vec<&str>) -> Result<i32> {
         let mut stack = Vec::new();
+        let mut pos = 0;
 
         while let Some(token) = tokens.pop(){
+            pos += 1;
+
             if let Ok(x) = token.parse::<i32>() {
                 stack.push(x);
             } else{
-                let y = stack.pop().expect("invalid syntax");
-                let x = stack.pop().expect("invalid syntax");
+                let y = stack.pop().context(format!("invalid syntax at {}", pos))?;
+                let x = stack.pop().context(format!("invalid syntax at {}", pos))?;
                 let res = match token {
                     "+" => x + y,
                     "-" => x - y,
                     "*" => x * y,
                     "/" => x / y,
                     "%" => x % y,
-                    _ => panic!("invalid token"),
+                    _ => bail!("invalid token at {}", pos),
                 };
                 stack.push(res);
             }
@@ -55,25 +60,22 @@ impl RpnCalculator{
             }
         }
 
-        if stack.len() == 1 {
-            stack[0]
-        }else{
-            panic!("invalid syntax")
-        }
+        ensure!(stack.len() == 1, "invalid syntax");
+
+        Ok(stack[0])
     }
 }
-fn main(){
+fn main() -> Result<()>{
     let opts = Opts::parse();
 
     if let Some(path) = opts.formula_file{
-        let f = File::open(path).unwrap();
+        let f = File::open(path)?;
         let reader = BufReader::new(f);
-        run(reader,opts.verbose);
+        run(reader,opts.verbose)
     }else{
         let stdin = stdin();
         let reader = stdin.lock();
-        run(reader,opts.verbose);
-        //println!("No file specified.");
+        run(reader,opts.verbose)
     }
     /*
     match opts.formula_file{
@@ -84,14 +86,18 @@ fn main(){
     */
 }
 
-fn run<R: BufRead>(reader: R, verbose : bool){
+fn run<R: BufRead>(reader: R, verbose : bool) -> Result<()>{
     let calc = RpnCalculator::new(verbose);
 
     for line in reader.lines(){
-        let line = line.unwrap();
-        let _answer = calc.eval(&line);
-        println!("{}", _answer);
+        let line = line?;
+        match calc.eval(&line){
+            Ok(answer) => println!("{}", answer),
+            Err(e) => eprintln!("{:#?}", e),
+        }
     }
+
+    Ok(())
 }
 
 #[cfg(test)]
